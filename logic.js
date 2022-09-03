fb23f16dad75245cc2bcf182aed49cfed80ffa1c
mohamedzaki20 <egymath2020@gmail.com>
2022-09-03 03:46:23 +0200

    escape keydown to hide the model

--- logic.js
+++ logic.js
@@ -2,16 +2,14 @@
 
 const showModel = document.querySelectorAll(".show-model");
 const closeModel = document.querySelector(".close-model");
-const hidden = document.querySelectorAll(".hidden");
-const model = hidden[0];
-const overlay = hidden[1];
+//const hidden = document.querySelectorAll(".hidden");
+const model = document.querySelector(".model");
+const overlay = document.querySelector(".overlay");
 
 // function to show model
 const show = function () {
   model.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  model.classList.add("model");
-  overlay.classList.add("overlay");
 };
 // function to hide model
 const hideModel = function () {
@@ -25,3 +23,10 @@ for (let i = 0; i < showModel.length; i++) {
 
 closeModel.addEventListener("click", hideModel);
 overlay.addEventListener("click", hideModel);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Escape") {
+    hideModel();
+    console.log(event);
+  }
+});
